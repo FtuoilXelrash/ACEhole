577e546426af1395ed60968af84a675182568f0b
Ftuoil_Xelrash <37750686+FtuoilXelrash@users.noreply.github.com>
2026-06-01 07:05:46 -0500

    Create AssemblyInfo.cs

---
+++ source/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACEhole")]
+[assembly: AssemblyDescription("ACE Asheron's Call Server Manager")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ftuoil Xelrash")]
+[assembly: AssemblyProduct("ACEhole")]
+[assembly: AssemblyCopyright("Copyright © 2026 Ftuoil Xelrash")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
